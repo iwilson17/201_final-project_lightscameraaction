fe850df419ec5918617e27b6516788e70996aec9
Isa <ifwilson@umich.edu>
2025-12-07 18:30:56 -0500

    edited database with nytimes

--- database.py
+++ database.py
@@ -76,6 +76,65 @@ def create_omdb_tables():
     conn.close()
 
 
+
+def create_nyt_table(): 
+    conn = sqlite3.connect("movies.db")
+    cur = conn.cursor()
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS nyt_articles (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            genre TEXT, 
+            headline TEXT, 
+            summary TEXT,
+            section TEXT, 
+            byline TEXT, 
+            date TEXT
+        )     
+    """)
+    conn.commit()
+    conn.close()
+
+
+def save_articles_by_genre_to_db(articles): 
+    conn = sqlite3.connect("movies.db")
+    cur = conn.cursor()
+
+    count_added = 0 
+
+    for article in articles: 
+        try: 
+            genre = article.get("genre")
+            if not genre: 
+                genre = "N/A"
+
+            headline = article.get("headline", "No Title")
+            pub_date = article.get("pub_date", "Unknown")
+            url = article.get("url", "N/A")
+
+            before = conn.total_changes
+            cur.execute("""
+                INSERT OR IGNORE INTO nyt_articles 
+                (genre, headline, pub_date, url)
+                VALUES (?, ?, ?, ?)
+            """, (genre, headline, pub_date, url))
+            after = conn.total_changes
+
+            if after > before: 
+                count_added += 1
+            
+            if count_added >= 24:
+                break 
+
+
+        except Exception as e:
+            print("Error saving NYT articles:", e)
+
+    conn.commit()
+    conn.close()
+    print("NYT: Added", count_added, "articles.")
+
+
 # --------------------------------------------------------
 # Save OMDB ratings (limit 25)
 # --------------------------------------------------------
@@ -122,6 +181,7 @@ def main():
 
     create_tmdb_tables()
     create_omdb_tables()
+    create_nyt_table()
 
     tmdb_movies = get_tmdb_movies()
     save_tmdb_to_db(tmdb_movies)
@@ -129,6 +189,10 @@ def main():
     imdb_ids = [m.get("imdb_id") for m in tmdb_movies if m.get("imdb_id")]
     omdb_movies = get_omdb_ratings(imdb_ids)
     save_omdb_to_db(omdb_movies)
+
+    nyt_articles = get_nyt_movie_articles()
+    save_articles_by_genre_to_db(nyt_articles)
+
 
     print("DONE.")
 
